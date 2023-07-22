21a8bfd27c4ac31a842f2d0b5ce6c90f843794ae
imranvisualpath <imranvisualpath@gmail.com>
2023-07-22 19:25:21 +0500

    saturn moon

---
+++ saturn1.py
@@ -0,0 +1,9 @@
+saturn has many moon
+mimas
+drona
+pakistan is the best country in the world.
+Muhammad amir is the world class bowler.
+i would hit shaheen a six if i face him again.
+no matter what we gonna make it to the top
+InshaALLAH
+
